402df4d1455fa7cbb35b64cc03d58c52b707379b
eduvicgar <162564929+eduvicgar@users.noreply.github.com>
2025-09-23 16:53:48 +0200

    first commit

---
+++ rest/app/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, jsonify, request
+import psycopg2
+import os
+
+app = Flask(__name__)
+app.config.from_prefixed_env()
+
+def get_db_connection():
+    with open('/run/secrets/postgres_password', 'r') as file:
+        db_password = file.read().strip()
+    conn = psycopg2.connect(
+        host=os.environ["PSQL_HOST"] ,
+        database=os.environ["PSQL_DB"],
+        user=os.environ["PSQL_USER"],
+        password=db_password
+    )
+    return conn
+
+@app.route('/')
+def index():
+    return "Hello, Flask with PostgreSQL!"
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM users;')
+    users = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(users)
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    new_user = request.json['name']
+    cursor.execute('INSERT INTO users (name) VALUES (%s);', (new_user,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return f"User {new_user} added successfully", 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
